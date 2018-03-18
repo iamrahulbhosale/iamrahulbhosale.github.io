5fe319bbee4ebf8e984a6d72ad29c82b9f9b5f7c
Tushar Kant <nanu.clickity@gmail.com>
2018-03-18 09:39:07 +0530

    live page

---
+++ src/client/pages/LivePage/PhotosSection/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+
+import round from 'lodash/round'
+
+import Sidebar, { SidebarLinks } from 'components/Sidebar'
+
+import s from './PhotosSection.styl'
+
+export default class LivePhotosSection extends Component {
+  componentDidMount = () => {
+    this._scalePortfolioImage = e => {
+      !this.isScaleImageBusy &&
+        window.requestAnimationFrame(() => {
+          this.isScaleImageBusy = true
+          this.scalePortfolioImage(e)
+        })
+    }
+    window.addEventListener('scroll', this._scalePortfolioImage)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this._scalePortfolioImage)
+  }
+  scalePortfolioImage = () => {
+    const dw = document.documentElement.clientWidth || window.innerWidth
+    const dh = document.documentElement.clientHeight || window.innerHeight
+    const bounds = this.container.getBoundingClientRect()
+    const factor = 0.8
+
+    if (dw < 768) {
+      return
+    }
+
+    var perc
+    if (bounds.top >= 0 && bounds.top < dh) {
+      perc = round(bounds.top / dh, 2)
+    } else {
+      perc = 0
+    }
+
+    const images = this.container.querySelectorAll('.photo-inner')
+
+    images.forEach(
+      (image, index) => (image.style.transform = `scale(${1 + perc})`)
+    )
+
+    this.isScaleImageBusy = false
+  }
+  render() {
+    const cx = classnames(s.container, 'photos-section', this.props.className)
+    return (
+      <div className={cx} ref={node => (this.container = node)}>
+        <Sidebar hasLogo>
+          <SidebarLinks />
+        </Sidebar>
+        <div className="sp-100" />
+        <div className="container">
+          <div className="page-title">
+            <span className="sw-rahul">RAHUL</span>
+            <span className="sw-dot">.</span>
+            <span className="sw-live">LIVE</span>
+          </div>
+          <div className="y-400" />
+          <div className="photo-item">
+            <div className="photo-inner" />
+            <div className="photo-name">Orange & Teal</div>
+          </div>
+          <div className="photo-item">
+            <div className="photo-inner" />
+            <div className="photo-name">Bouncing Sun</div>
+          </div>
+          <div className="photo-item">
+            <div className="photo-inner" />
+            <div className="photo-name">Bouncing Sun</div>
+          </div>
+          <div className="photo-item">
+            <div className="photo-inner" />
+            <div className="photo-name">Study</div>
+          </div>
+        </div>
+        <div className="sp-100" />
+      </div>
+    )
+  }
+}
--- src/client/pages/LivePage/index.js
+++ src/client/pages/LivePage/index.js
@@ -2,12 +2,50 @@ import React, { Component } from 'react'
 import s from './LivePage.styl'
 
 import HeroSection from './HeroSection'
+import PhotosSection from './PhotosSection'
 
 export default class LivePage extends Component {
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const dw = document.documentElement.clientWidth || window.innerWidth
+    if (dw < 768) {
+      return
+    }
+    if (this.isLayoutBusy) {
+      return
+    }
+    window.requestAnimationFrame(this.updateLayout)
+    this.isLayoutBusy = true
+  }
+
+  updateLayout = e => {
+    const sections = Array.from(document.querySelectorAll('.cd-section'))
+    sections.forEach(s => {
+      const innerHeight = s
+        .querySelector('.page-section')
+        .getBoundingClientRect().height
+      s.style.height = `${innerHeight}px`
+      // s.classList.toggle('stuck', this.isInViewport(s))
+    })
+    this.isLayoutBusy = false
+  }
+
   render() {
     return (
       <div className={s.container}>
-        <HeroSection />
+        <div className="cd-section stuck">
+          <HeroSection className="page-section" />
+        </div>
+        <div className="cd-section">
+          <PhotosSection className="page-section" />
+        </div>
       </div>
     )
   }
