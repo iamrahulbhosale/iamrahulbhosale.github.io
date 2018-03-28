90564e24d4eee4d533c66d3937dd10e958c7f154
Tushar Kant <nanu.clickity@gmail.com>
2018-03-28 19:05:45 +0530

    updates

--- src/client/pages/HomePage/HeroSection/index.js
+++ src/client/pages/HomePage/HeroSection/index.js
@@ -5,32 +5,43 @@ import { Link } from 'react-router-dom'
 
 import Sidebar, { SidebarLinks } from 'components/Sidebar'
 
-import { TRANSITION_END_EVENT } from 'utils/constants'
-
+// import { TRANSITION_END_EVENT } from 'utils/constants'
 import s from './HeroSection.styl'
 
+const PROFILE_IMAGE = '/public/img/profile-image.jpg'
+
 export default class HeroSection extends Component {
+  state = {
+    imageLoaded: false
+  }
   componentDidMount = () => {
-    this.animate()
-    this.onTextAnimationComplete()
-    // setTimeout(this.animate, 400)
-    // console.log('will use event: ', TRANSITION_END_EVENT)
+    this.waitForImage(PROFILE_IMAGE)
+      .then(() => this.setState({ imageLoaded: true }))
+      .then(() => this.startAnimations())
   }
 
-  animate = () => {
+  waitForImage = imageUrl => {
+    if (!imageUrl) {
+      return Promise.reject(`'imageUrl' not supplied`)
+    }
+    const img = document.createElement('img')
+    return new Promise((resolve, reject) => {
+      img.onload = function() {
+        resolve()
+      }
+      img.onerror = function() {
+        reject()
+      }
+      img.src = imageUrl
+    })
+  }
+
+  startAnimations = () => {
     const texts = Array.from(this.container.querySelectorAll('.hero-text'))
-    // const lastText = texts[0]
+    const image = this.container.querySelector('.profile-image')
 
-    // TRANSITION_END_EVENT.split(' ').forEach(evt => {
-    //   lastText.addEventListener(evt, this.onTextAnimationComplete)
-    // })
-
-    texts.forEach(x => x.classList.add('will-animate'))
-  }
-
-  onTextAnimationComplete = e => {
-    console.log('transition done')
-    this.container.querySelector('.profile-image').classList.add('will-animate')
+    texts.map(x => x.classList.add('will-animate'))
+    image.classList.add('will-animate')
   }
 
   render() {
@@ -39,20 +50,31 @@ export default class HeroSection extends Component {
       'hero-section bg-dark',
       this.props.className
     )
+
+    const imageStyle = this.state.imageLoaded
+      ? { backgroundImage: `url(${PROFILE_IMAGE})` }
+      : {}
+
     return (
       <div className={cx} ref={node => (this.container = node)}>
         <Sidebar hasLogo white>
           <SidebarLinks />
         </Sidebar>
         <div className="container hero-text-container">
-          <span className="hero-text">I’m a designer who believes design</span>
-          <span className="hero-text">thinking can solve most problems.</span>
           <span className="hero-text">
-            Philosophy:&nbsp;
-            <span className="highlight">Good design is obvious.</span>
+            <div class="text-inner">I’m a designer who believes design</div>
+          </span>
+          <span className="hero-text">
+            <div class="text-inner">thinking can solve most problems.</div>
+          </span>
+          <span className="hero-text">
+            <div class="text-inner">
+              Philosophy:&nbsp;
+              <span className="highlight">Good design is obvious.</span>
+            </div>
           </span>
         </div>
-        <div className="profile-image" />
+        <div className="profile-image" style={imageStyle} />
         <div className="go-down-button hide-offscreen">
           <img className="img-fluid" alt="" src="/public/img/arrow-down.gif" />
         </div>
