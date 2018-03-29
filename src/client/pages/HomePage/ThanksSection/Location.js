ef5f799d21fbb46aec4983d9496671b46e5d620a
iamrahulbhosale <rahulgraphy@gmail.com>
2018-03-29 15:42:55 +0530

    rahul's changes

--- src/client/pages/HomePage/ThanksSection/Location.js
+++ src/client/pages/HomePage/ThanksSection/Location.js
@@ -5,12 +5,12 @@ import Logo from 'components/ui/Logo'
 export const Location = props => (
   <div className="location-container">
     <div className="logo-wrapper">
-      <Logo white className='black-square'/>
+      <Logo white className='black-square' />
     </div>
     <div className="location-details">
-      <div className="location-label">Let's meet, i'm currently in</div>
+      <div className="location-label">Let's meet, I'm currently in</div>
       <div className="location-value">
-        Mumbai
+        Pune
         <span className="location-emoji">🌤</span>
       </div>
     </div>
